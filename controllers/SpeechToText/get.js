28f8bf22889ff0e4482737fbf02aa76c0f7f5e1a
Rohit Pal <prohit9370@gmail.com>
2023-12-08 21:44:36 +0530

    Configured file upload for vercel servelerless function

--- controllers/SpeechToText/get.js
+++ controllers/SpeechToText/get.js
@@ -1,5 +1,6 @@
 const OpenAI = require("openai");
 const fs = require("fs");
+const path = require("path");
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
@@ -9,14 +10,21 @@ class getTranscript {
       if (!req.file) {
         return res.status(400).json({ error: "No file uploaded" });
       }
-      const filePath = req.file.path;
+      // Write the file to disk
+      const filePath = path.join(__dirname, req.file.originalname);
+      fs.writeFileSync(filePath, req.file.buffer);
+
       const transcription = await openai.audio.transcriptions.create({
         file: fs.createReadStream(filePath),
         model: "whisper-1"
       });
 
+      // Delete the file after processing
+      fs.unlinkSync(filePath);
+
       res.status(200).json(transcription.text);
     } catch (error) {
+      console.log("🚀 ~ error:", error);
       res.status(400).json(error);
     }
   };
