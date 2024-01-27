9f518baa0cf9b4fa0d24779a6805e3e02e05ede6
Rohitpal0123 <prohit9370@gmail.com>
2024-01-27 12:56:29 +0530

    hardcode cors domain

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://sahil1gupta.github.io", // replace with your client's origin
+  }),
+);
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
