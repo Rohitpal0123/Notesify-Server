3dc32ecff315559e388397edd1166af6b2f6fde0
Rohit Pal <prohit9370@gmail.com>
2024-02-23 14:49:18 +0530

    added notesify-three link to cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 8000;
 
 app.use(
   cors({
-    origin: "https://notesify-server.vercel.app",
+    origin: "https://notesify-three.vercel.app",
     credentials: true,
   })
 );
